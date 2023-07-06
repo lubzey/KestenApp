31a4a33ea903929479bd64138868c26509bbb156
Lyubomir Durankev <lubzdudz@gmail.com>
2023-07-06 13:16:55 +0300

    Added sample properties

--- Models/Varieties/VarietyDetailsModel.cs
+++ Models/Varieties/VarietyDetailsModel.cs
@@ -1,5 +1,4 @@
 ﻿using KestenApp.Data.Models;
-using System.Collections.ObjectModel;
 
 namespace KestenApp.Models.Varieties
 {
@@ -8,8 +7,8 @@ namespace KestenApp.Models.Varieties
         public int VarietyId;
         public string VarietyName;
         public string Description;
-        //public Variety Variety;
-        public IEnumerable<KeyValuePair<string, string>> Tree;
+        public List<KeyValuePair<string, string>> Tree = new List<KeyValuePair<string, string>>();
+        public List<KeyValuePair<string, string>> Fruit = new List<KeyValuePair<string, string>>();
         public IList<CheckboxModel> SpeciesCheckboxes;
         public string ThumbnailImagePath;
 
@@ -19,24 +18,61 @@ namespace KestenApp.Models.Varieties
             VarietyName = variety.VarietyName;
             Description = variety.Description;
 
-            //Variety = variety;
-            Tree = new List<KeyValuePair<string, string?>>
-            {
-                new KeyValuePair<string, string?>("Pollen", variety.PollenType.ToString()),
-                new KeyValuePair<string, string?>("Vigor", null),
-                new KeyValuePair<string, string?>("Erect", null),
-                new KeyValuePair<string, string?>("Blight Resistance", variety.ChestnutBlightResistance.ToString()),
-                new KeyValuePair<string, string?>("Ink Disease Resistance", variety.InkDiseaseResistance.ToString()),
-            }
-                .Where(kvp => !string.IsNullOrEmpty(kvp.Value))
-                .Select(kvp => new KeyValuePair<string, string>(kvp.Key, kvp.Value ?? ""))
-                .ToList();
+            //Tree
+            AddKeyValuePairToCollection(Tree, "Pollen", variety.PollenType.ToString());
+            AddKeyValuePairToCollection(Tree, "Vigor", null);
+            AddKeyValuePairToCollection(Tree, "Budding Period", null);
+            AddKeyValuePairToCollection(Tree, "Flowering Period", null);
+            AddKeyValuePairToCollection(Tree, "Maturity Period", null);
+            AddKeyValuePairToCollection(Tree, "Blight Resistance", variety.ChestnutBlightResistance.ToString());
+            AddKeyValuePairToCollection(Tree, "Ink Disease Resistance", variety.InkDiseaseResistance.ToString());
+            AddKeyValuePairToCollection(Tree, "Gall Wasp Resistance", variety.InkDiseaseResistance.ToString());
+            AddKeyValuePairToCollection(Tree, "Cold Hardiness", null);
+            AddKeyValuePairToCollection(Tree, "Spring Frost Sensitivity", null);
+            AddKeyValuePairToCollection(Tree, "Kernel Rot Susceptibility", null);
+            AddKeyValuePairToCollection(Tree, "Catkins Type", null);
+            AddKeyValuePairToCollection(Tree, "Crop Volume", null);
 
+            //Rootstock and pollentation
+
+            //Fruit
+            AddKeyValuePairToCollection(Fruit, "Taste", "Good");
+            AddKeyValuePairToCollection(Fruit, "Size", string.Join(", ", variety.FruitSizes.Select(s => s.Name)));
+            AddKeyValuePairToCollection(Fruit, "Is Marron", GetStringFromNullableBoolean(variety.IsMarron)); //<12%
+            AddKeyValuePairToCollection(Fruit, "Kernel Splitting", "None,some,many?");
+            AddKeyValuePairToCollection(Fruit, "Kernel Intrusion", "Deep,Shallow,None");
+            AddKeyValuePairToCollection(Fruit, "Shape", "Oval, Triangular, Irregular ???");
+            AddKeyValuePairToCollection(Fruit, "Shell Color", "Dark brown, Brown, Red, Orange, Yellow ???");
+            AddKeyValuePairToCollection(Fruit, "Kernel Color", "Milky White, Dark Yellow?");
+            AddKeyValuePairToCollection(Fruit, "Peeling", variety.Peeling.ToString());
+            AddKeyValuePairToCollection(Fruit, "Conservation", "Medium");
+            AddKeyValuePairToCollection(Fruit, "Nuts per Bur", "3");
+            AddKeyValuePairToCollection(Fruit, "Hilum Size", "Large");            
+            AddKeyValuePairToCollection(Fruit, "Shell Type", "Fuzzy, Shiny?");
 
             SpeciesCheckboxes = speciesCheckboxes;
             ThumbnailImagePath = variety.Images.Any()
                 ? $"/Images/Varieties/{variety.VarietyId}/{variety.Images.First().FileName}.jpg"
                 : $"/Images/no-image.jpg";
+        }
+
+        private void AddKeyValuePairToCollection(List<KeyValuePair<string, string>> collection, string key, string? value)
+        {
+            //if (string.IsNullOrEmpty(value))
+            //{
+            //    return;
+            //}
+
+            collection.Add(new KeyValuePair<string, string>(key, value ?? "None"));
+        }
+
+        private static string GetStringFromNullableBoolean(bool? isMarron)
+        {
+            return isMarron != null
+                ? (bool)isMarron
+                    ? "\u2713"
+                    : "\u2717"
+                : "";
         }
     }
 }
